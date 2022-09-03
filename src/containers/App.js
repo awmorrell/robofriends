ff49630c7f2c6e3142e22bf4d58a717149e019b7
awmorrell <amorrell424@gmail.com>
2022-09-02 17:46:36 -0700

    pushing robofriends proj

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/SearchBox.js';
+import { robots } from '../robots.js'; //must destructure imports that arent exported as default
+import './App.css';
+import Scroll from '../components/Scroll.js';
+
+class App extends Component { //unlike the other components, App is a 'smart component'. The other components are simple functions, this one has state.
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		};
+	};
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => {
+				return response.json();
+			})
+			.then(users => {
+				this.setState({ robots: robots })
+			})
+	};
+
+	onSearchChange = (event) => { // writing it this way makes it so that the value of this is the App, rather than the input (event). }
+		this.setState({ searchfield: event.target.value }) // have to add this.setState to change the state of the designated object, and in the brackets put what we want to change the state to}
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={ this.onSearchChange }/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'; //React has webpack underneath the hood, so we can import it straight in without having to bundle or browserify
+import ReactDOM from 'react-dom/client'; //ReactDOM is the glue that puts React together in web browsers specifically. ReactNative is for mobile apps, ReactVR for vr apps, etc
+import './index.css'; //React allows us to add different CSS files to each different component. / means it is located in the same folder.
+import App from './containers/App.js';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
