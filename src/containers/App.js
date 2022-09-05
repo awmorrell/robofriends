87f2b2a00a364ba12dd8fb68dd66907bd15b1de3
awmorrell <amorrell424@gmail.com>
2022-09-04 19:26:46 -0700

    added error boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
+import ErrorBoundary from '../components/ErrorBoundary.js';
 import { robots } from '../robots.js'; //must destructure imports that arent exported as default
 import './App.css';
 import Scroll from '../components/Scroll.js';
@@ -37,7 +38,9 @@ class App extends Component { //unlike the other components, App is a 'smart com
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={ this.onSearchChange }/>
 				<Scroll>
-					<CardList robots={filteredRobots}/>
+					<ErrorBoundary>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundary>
 				</Scroll>
 			</div>
 		);
