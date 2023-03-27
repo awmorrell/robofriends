d453f29f86d197646696b429c79af9c07c65f68c
awmorrell <amorrell424@gmail.com>
2023-03-26 21:45:39 -0700

    added service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'; //React has webpack underneath the hood, so we can im
 import ReactDOM from 'react-dom/client'; //ReactDOM is the glue that puts React together in web browsers specifically. ReactNative is for mobile apps, ReactVR for vr apps, etc
 import './index.css'; //React allows us to add different CSS files to each different component. / means it is located in the same folder.
 import App from './containers/App.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
@@ -22,6 +23,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
