3fd88c3e5cd6d76cca484050e1ef5a7c5c30cb1c
awmorrell <amorrell424@gmail.com>
2023-03-28 20:47:36 -0700

    PWA optimizations

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import './Card.css';
 const Card = ({ name, email, id }) => {
 	return (
 		<div id='cardz' className='tc dib br3 pa3 ma2 mt3 grow bw2 shadow-5'>
-			<img alt='robot' src={`https://robohash.org/${id}1?200x200`} /> 
+			<img alt='robot' src={`https://robohash.org/${id}1?size=200x200`} /> 
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
